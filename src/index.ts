d315a3343ee1b956e00b694802fbc103b46e7762
darunbjork <darunbjork@gmail.com>
2026-01-16 02:56:54 +0100

    Update src/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,42 @@
 import fastify from "fastify";
 
+let messages = [
+  {
+    id: 1,
+    user: 'Alice',
+    text: 'Hello!',
+    createdAt: new Date().toISOString(),
+  },
+  {
+    id: 2,
+    user: 'Bob',
+    text: 'Hello!',
+    createdAt: new Date().toISOString()
+  }
+]
+
 const server = fastify();
 
 server.get('/messages', async () => {
- return [
-    { id: 1, user: 'Alice', text: 'Hello!' },
-    { id: 2, user: 'Bob', text: 'Hi there!' },
-  ];
+ return messages;
+});
+
+server.post('/messages', async (request) => {
+  const body = request.body as {
+    user: string;
+    text: string;
+  };
+
+  const newMessages = {
+    id: messages.length + 1,
+    user: body.user,
+    text: body.text,
+    createdAt: new Date().toISOString(),
+  };
+
+  messages.push(newMessages);
+
+  return newMessages;
 });
 
 server.listen({ port: 3000}, () => {
